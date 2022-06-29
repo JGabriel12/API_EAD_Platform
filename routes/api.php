caae67f42f8cfd91b84fee0949829cf80c2e0aef
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-06-29 18:22:08 -0300

    Migrations update / Controller Course

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\{
+    CourseController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/courses', [CourseController::Class, 'index']);
 
 Route::get('/', function () {
     return response()->json([
