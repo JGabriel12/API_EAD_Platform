8e3a717430edb0bda838bb6b794eb759fe6bf73f
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-07-01 00:34:36 -0300

    Rout find courses / Repository Course

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Api\{
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::Class, 'index']);
+Route::get('/courses', [CourseController::class, 'index']);
+
+Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/', function () {
     return response()->json([
