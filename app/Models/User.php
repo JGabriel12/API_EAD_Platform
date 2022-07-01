64d67e623f92dc05d82a1093a15e14515c60ed62
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-06-30 23:46:15 -0300

    keyType uuid fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, UuidTrait;
 
+    public $increments = false;
+    protected $keyType = 'uuid';
+
     /**
      * The attributes that are mass assignable.
      *
