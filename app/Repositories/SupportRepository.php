84500116b7fff24871686865687e52d47a2fef3e
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-07-12 16:57:21 -0300

    StoreSupport

--- app/Repositories/SupportRepository.php
+++ app/Repositories/SupportRepository.php
@@ -38,4 +38,17 @@ class SupportRepository
     //return auth()->user();
     return User::first();
   }
+
+  public function createNewSupport(array $data): Support
+  {
+    $support = $this->getUserAuth()
+      ->supports()
+      ->create([
+        'lesson_id' => $data['lesson'],
+        'description' => $data['description'],
+        'status' => $data['status'],
+      ]);
+
+    return $support;
+  }
 }
