8d1da447660b51f1af0f020249f700f4d07e652c
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-07-13 17:14:04 -0300

    EndPoints Authorization / generating Auth token

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,29 @@ use App\Http\Controllers\Api\{
     ModuleController,
     SupportController
 };
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::middleware(['auth:sanctum'])->group(function () {
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    Route::get('/courses', [CourseController::class, 'index']);
 
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
 
-Route::get('/my-supports', [SupportController::class, 'mySupports']);
-Route::get('/supports', [SupportController::class, 'index']);
-Route::post('/supports', [SupportController::class, 'store']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
+    Route::get('/my-supports', [SupportController::class, 'mySupports']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::post('/supports', [SupportController::class, 'store']);
+
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
 
 Route::get('/', function () {
     return response()->json([
